cc9b6b11bc1c2d76ddfd016034c9a6403a55e3db
Shauryaditya <shauryadityab@gmail.com>
2022-09-19 14:52:35 +0530

    Fileupload

---
+++ home/views.py
@@ -0,0 +1,38 @@
+from tkinter import E
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
+
+from .serializers import *
+
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def download(request , uid):
+    return render(request , 'download.html', context = {'uid' : uid})
+
+class HandleFileUpload(APIView):
+    parser_classes = [MultiPartParser]
+    def post(self , request):
+        try:
+            data = request.data
+
+            serializer = FileListSerializer(data = data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'status': 200,
+                    'message' : 'files uploaded succesfuly'
+                })
+
+            return Response({
+                'status' : 400,
+                'message' : 'something went wrong',
+                'data' : serializer.errors
+            })
+        except Exception as e:
+            print(e)
